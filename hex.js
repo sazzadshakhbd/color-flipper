85e0a54d0988f594bfe8d6a7038ae531453f7f3b
netcodensazzadali <netcodensazzadali@gmail.com>
2023-10-26 23:16:47 +0600

    randomly unlimited color change

---
+++ hex.js
@@ -0,0 +1,16 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector('.color');
+document.getElementById('btn').addEventListener('click', function(){
+    let hexColor = '#';
+    for(let i = 0; i < 6; i++) {
+        hexColor += hex[getHexColor()]
+    }
+
+    color.textContent  = hexColor;
+    document.body.style.backgroundColor = hexColor
+})
+
+function getHexColor() {
+    return Math.floor(Math.random() * hex.length)
+}
